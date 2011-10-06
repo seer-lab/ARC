adaa5ff8e1d32267713b6010460bfe6590e3ec3d
kevinjalbert <kevin.j.jalbert@gmail.com>
2011-10-06 13:46:07 -0400

    Add mutation operator variables in the config.py
    Add:
     * Ability to toggle operators within the config.py
     * The TXL file location of the mutation operators in config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """This module holds all the configuration values that are used in ARC.
 
 There are system, target project, ConTest, mutation operator and Pyevolve
-variables that are set in this file and are used all throughout ARC.  
+variables that are set in this file and are used all throughout ARC.
 """
 
 # System variables
@@ -28,6 +28,30 @@ _CONTEST_RUNS = 25
 _CONTEST_TIMEOUT_SEC = 2  # Aim for around x2-3 desirable performance
 _TESTSUITE_AVG = 20  # Number of test executions for finding the average time
 
-# Mutation operator variables
+# Mutation operator variables (True == considered as an operator)
+_MUTATIONS_ENABLE = {'ASAS': True,
+                     'ASAV': True,
+                     'ASM': True,
+                     'CSO': True,
+                     'EXCR': True,
+                     'EXSA': True,
+                     'RSAS': True,
+                     'RSAV': True,
+                     'RSB': True,
+                     'RSM': True,
+                     'SHSA': True,
+                     'SHSB': True}
+_MUTATIONS_FILE = {'ASAS': _ROOT_DIR + "src/_txl/ASAS.Txl",
+                   'ASAV': _ROOT_DIR + "src/_txl/ASAV.Txl",
+                   'ASM': _ROOT_DIR + "src/_txl/ASM.Txl",
+                   'CSO': _ROOT_DIR + "src/_txl/CSO.Txl",
+                   'EXCR': _ROOT_DIR + "src/_txl/EXCR.Txl",
+                   'EXSA': _ROOT_DIR + "src/_txl/EXSA.Txl",
+                   'RSAS': _ROOT_DIR + "src/_txl/RSAS.Txl",
+                   'RSAV': _ROOT_DIR + "src/_txl/RSAV.Txl",
+                   'RSB': _ROOT_DIR + "src/_txl/RSB.Txl",
+                   'RSM': _ROOT_DIR + "src/_txl/RSM.Txl",
+                   'SHSA': _ROOT_DIR + "src/_txl/SHSA.Txl",
+                   'SHSB': _ROOT_DIR + "src/_txl/SHSB.Txl"}
 
 # Pyevolve variables
